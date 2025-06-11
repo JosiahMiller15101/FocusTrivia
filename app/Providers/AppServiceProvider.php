e253b5826a43914372fc95040454bde566842fa5
Josiah Miller <Josiah.Miller@focus.org>
2025-06-11 17:11:02 -0600

    your message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
-       if (config('app.env') === 'production') {
-        URL::forceScheme('https');
-    }
+        // Force HTTPS in production
+        // if (config('app.env') === 'production') {
+        //     URL::forceScheme('https');
+        // }
     }
 }
