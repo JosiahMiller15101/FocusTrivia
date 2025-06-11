b799f11fb353653874bbd04ac03355412f5f3f11
Josiah Miller <Josiah.Miller@focus.org>
2025-06-11 15:42:01 -0600

    your message

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+// Use the appropriate base class depending on your Laravel version:
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array|string|null
+     */
+    protected $proxies = '*'; // Trust all proxies (like Render)
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+}
