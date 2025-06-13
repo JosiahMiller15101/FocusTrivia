4ae404f156699f4d7b4e908c282203a6c8cc8319
Josiah Miller <Josiah.Miller@focus.org>
2025-06-13 14:39:07 -0600

    Add Google Socialite login routes and config

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -76,12 +76,12 @@
     <a href="/" class="text-sm/6 font-semibold text-gray-900 shadow-sm">Cancel</a>
     <x-form-button>Register</x-form-button>
   </div>
-  <div class="mt-6 flex items-center justify-center">
+<div class="mt-6 flex items-center justify-center gap-x-4">
     <a href="/login/google" class="inline-flex items-center px-4 py-2 bg-indigo-600 text-sm font-semibold text-white rounded-md shadow-lg hover:bg-indigo-500 focus-visible:outline-indigo-600">
         <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 24 24"><path d="M21.805 10.023h-9.765v3.977h5.617c-.242 1.242-1.484 3.648-5.617 3.648-3.383 0-6.148-2.797-6.148-6.25s2.765-6.25 6.148-6.25c1.93 0 3.227.82 3.969 1.523l2.711-2.633c-1.711-1.57-3.906-2.523-6.68-2.523-5.523 0-10 4.477-10 10s4.477 10 10 10c5.742 0 9.547-4.023 9.547-9.695 0-.652-.07-1.148-.156-1.652z"/></svg>
         Sign up with Google
     </a>
-      <a href="/login/youtube" class="inline-flex items-center px-4 py-2 bg-red-600 text-sm font-semibold text-white rounded-md shadow-lg hover:bg-red-700 focus-visible:outline-red-600">
+    <a href="/login/youtube" class="inline-flex items-center px-4 py-2 bg-red-600 text-sm font-semibold text-white rounded-md shadow-lg hover:bg-red-700 focus-visible:outline-red-600">
         <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 24 24"><path d="M21.8 8.001c-.2-1.5-1.5-2.7-3-2.9C16.2 4.7 12 4.7 12 4.7s-4.2 0-6.8.4c-1.5.2-2.8 1.4-3 2.9C2 9.6 2 12 2 12s0 2.4.2 3.999c.2 1.5 1.5 2.7 3 2.9C7.8 19.3 12 19.3 12 19.3s4.2 0 6.8-.4c1.5-.2 2.8-1.4 3-2.9.2-1.6.2-3.999.2-3.999s0-2.4-.2-3.999zM9.8 15.2V8.8l6.4 3.2-6.4 3.2z"/></svg>
         Sign up with YouTube
     </a>
--- resources/views/components/form-button.blade.php
+++ resources/views/components/form-button.blade.php
@@ -1,4 +1,4 @@
-<button {{ $attributes->merge(['class' => 'rounded-md bg-slate-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-slate-600 focus-visible:outline-slate-500', 'type' => 'submit']) }}>
+<button {{ $attributes->merge(['class' => 'rounded-md bg-slate-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline-slate-500', 'type' => 'submit']) }}>
     {{$slot}}
 </button>
 
