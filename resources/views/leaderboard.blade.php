6e448bd62a9600d6040f646b3e3cedf929e40c2f
Josiah Miller <Josiah.Miller@focus.org>
2025-07-14 15:49:40 -0600

    search filter

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -157,7 +157,7 @@
                 <img src="{{ asset('images/events.png') }}" alt="Events Department" class="w-7 h-7 rounded-full object-cover border border-gray-300">
               @endif
               @if(strtolower(trim($dept['department'])) == 'it')
-                <img src="{{ asset('images/it.png') }}" alt="IT Department" class="w-7 h-7 rounded-full object-cover border border-gray-300">
+                <img src="{{ asset('images/IT.png') }}" alt="IT Department" class="w-7 h-7 rounded-full object-cover border border-gray-300">
               @endif
               @if(strtolower(trim($dept['department'])) == 'hr')
                 <img src="{{ asset('images/HR.png') }}" alt="HR Department" class="w-7 h-7 rounded-full object-cover border border-gray-300">
